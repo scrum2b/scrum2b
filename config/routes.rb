250a47fef5606bab4a2ed78346ae8bf46f58840b
Ho Anh Tuan <tuanha@ithanoi.com>
2012-10-18 14:26:21 +0700

    all

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-  resources :issue
-  get 'scrum2b', :to => 'issue#index'
+resources :issue
+match "issues/lists" => "issue#index"
+
+ 
+  
