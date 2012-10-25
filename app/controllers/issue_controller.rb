3067c8ca30efb5f9748c7bccf09d95bc011cec1d
Ho Anh Tuan <tuanha@ithanoi.com>
2012-10-25 16:28:18 +0700

    search issue

--- app/controllers/issue_controller.rb
+++ app/controllers/issue_controller.rb
@@ -1,24 +1,53 @@
 class IssueController < ApplicationController
   unloadable
-  before_filter :find_project, :authorize, :only => :index
- 
+  before_filter :find_project, :only => [:index,:list]
   def index
-   @list_version = @project.versions.all
-   @id_version  = nil
-   if @id_version
-     @version = Version.where(:id => @id_version);
-     
-   elsif
+    @list_versions = @project.versions.all
     @version = @project.versions.all
-   end
-   @issues_backlog = @project.issues.where(:fixed_version_id => nil ).all
-  
+    @issues_backlog = @project.issues.where(:fixed_version_id => nil ).all
+
   end
 
+  def list
+    @list_versions = @project.versions.all
+    @id_version  = params[:select_version]
+    @select_issues  = params[:select_issue]
 
-  
+    if @select_issues
+      if @select_issues == "1"
+        @issues =  @project.issues.all
+      end
+      if @select_issues == "2"
+        @issues =  @project.issues.where(:assigned_to_id => User.current.id)
+      end
+      if @select_issues == "3"
+        @issues =  @project.issues.where(:assigned_to_id => User.current.id , :done_ratio => 100)
+      end
+      if @select_issues == "4"
+        @issues =  @project.issues.order("created_on DESC").limit(20)
+      end
+      if @select_issues == "5"
+        @issues =  @project.issues.where(:done_ratio => 100 ).all
+      end
+    elsif
+    @issues = @project.issues.all
+    end
+
+    if @id_version
+      if @id_version == "all"
+        @version = @project.versions.all
+
+      elsif
+      @version = Version.where(:id => @id_version);
+      end
+    elsif
+    @version = @project.versions.all
+    end
+    @issues_backlog = @project.issues.where(:fixed_version_id => nil ).all
+  end
+
   private
-  
+
   def find_project
     # @project variable must be set before calling the authorize filter
     @project = Project.find(params[:project_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-
- 
-  
+resources :issue
+match '/issue/list' => 'issue#list'
