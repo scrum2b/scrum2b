af83c124a1b9ef5eb646ceefe039e387ff9bfd81
Kieu Viet Hung <hungkv@ithanoi.com>
2012-11-12 09:57:30 +0700

    still working

--- app/controllers/issue_controller.rb
+++ app/controllers/issue_controller.rb
@@ -3,7 +3,6 @@ class IssueController < ApplicationController
   before_filter :find_project, :only => [:index,:board]
 
   before_filter :set_status_settings
-
   def index
     if session[:view_mode]&& session[:view_mode] = "board" && !params[:view_mode]
       session[:view_mode] = "board"
@@ -12,7 +11,7 @@ class IssueController < ApplicationController
       session[:view_mode] = "list"
     end
     if !session[:view_mode] || params[:view_mode] && params[:view_mode] == "List"
-    session[:view_mode] = "list"
+      session[:view_mode] = "list"
     end
     @list_versions = @project.versions.all
     @id_version  = params[:select_version]
@@ -85,7 +84,11 @@ class IssueController < ApplicationController
     @version.each do |version|
       @id_version.push(version.id)
     end
-    @issues_select = @issues.where(:fixed_version_id => @id_version)
+    if @id_version == []
+      @issues_select = @issues
+    else
+      @issues_select = @issues.where(:fixed_version_id => @id_version)
+    end
     @issues_new = @project.issues.where(:status_id => @default_not_start_status_id.to_i)
     @issues_completed = @issues_select.where(:done_ratio => 100)
     @issues_started = @issues_select.where(:status_id => @default_inprogress_status_id.to_i)
@@ -93,34 +96,35 @@ class IssueController < ApplicationController
 
   def update_status
     @project =  Project.find(params[:project_id])
-   if params[:status] == "sortable3"
-    @issue = @project.issues.find(params[:issue_id])
-    @issue.update_attribute(:done_ratio,100)
-    @issue.update_attribute(:status_id,@default_completed_status_id.to_i)
+    if params[:status] == "sortable3"
+      @issue = @project.issues.find(params[:issue_id])
+      @issue.update_attribute(:done_ratio,100)
+      @issue.update_attribute(:status_id,@default_completed_status_id.to_i)
     end
     if params[:status] == "sortable2"
-    @issue = @project.issues.find(params[:issue_id])
-    @issue.update_attribute(:status_id,@default_inprogress_status_id.to_i)
-    if @issue.done_ratio = 100
-        @issue.update_attribute(:done_ratio,90)
-    end
+      @issue = @project.issues.find(params[:issue_id])
+      @issue.update_attribute(:status_id,@default_inprogress_status_id.to_i)
+      if @issue.done_ratio = 100
+        @issue.update_attribute(:done_ratio,0)
+      end
     end
     if params[:status] == "sortable1"
-    @issue = @project.issues.find(params[:issue_id])
-    @issue.update_attribute(:status_id,@default_not_start_status_id.to_i)
+      @issue = @project.issues.find(params[:issue_id])
+      @issue.update_attribute(:status_id,@default_not_start_status_id.to_i)
     end
   end
 
   def ajax
-   @project =  Project.find(params[:project_id])
-   #@issue = @project.issues.where(:id => params[:issue_id])
+    @project =  Project.find(params[:project_id])
+    #@issue = @project.issues.where(:id => params[:issue_id])
     @issue = @project.issues.find(params[:issue_id])
     @issue.update_attribute(:done_ratio, params[:done_ratio])
     Rails.logger.info "test object: #{@issue.done_ratio.to_s}"
   end
+
   def ajax
-   @project =  Project.find(params[:project_id])
-   #@issue = @project.issues.where(:id => params[:issue_id])
+    @project =  Project.find(params[:project_id])
+    #@issue = @project.issues.where(:id => params[:issue_id])
     @issue = @project.issues.find(params[:issue_id])
     @issue.update_attribute(:done_ratio, params[:done_ratio])
     Rails.logger.info "test object: #{@issue.done_ratio.to_s}"
@@ -132,43 +136,42 @@ class IssueController < ApplicationController
     # @project variable must be set before calling the authorize filter
     @project = Project.find(params[:project_id])
   end
-  
+
   def set_status_settings
     @plugin = Redmine::Plugin.find("scrum2b")
     @settings = Setting["plugin_#{@plugin.id}"]
-    
+
     @not_start_statuses_id = []
-    if @settings['status_no_start']  
-    @settings['status_no_start'].each do |setting|
-     @not_start_statuses_id.push(setting[0])
-     end
-     @default_not_start_status_id = @not_start_statuses_id[0]
-     
+    if @settings['status_no_start']
+      @settings['status_no_start'].each do |setting|
+        @not_start_statuses_id.push(setting[0])
+      end
+      @default_not_start_status_id = @not_start_statuses_id[0]
+
     end
-    
+
     @inprogress_statuses_id = []
     if @settings['status_inprogress'].each do |setting|
-      @inprogress_statuses_id.push(setting[0])
+    @inprogress_statuses_id.push(setting[0])
     end
       @default_inprogress_status_id = @inprogress_statuses_id[0]
     end
-    
+
     @completed_statuses_id = []
     if @settings['status_completed']
       @settings['status_completed'].each do |setting|
         @completed_statuses_id.push(setting[0])
       end
-     @default_completed_status_id = @completed_statuses_id[0]
+      @default_completed_status_id = @completed_statuses_id[0]
     end
-    
+
     @closed_statuses_id = []
     if @settings['status_closed']
       @settings['status_closed'].each do |setting|
         @closed_statuses_id.push(setting[0])
       end
-     @default_closed_status_id = @closed_statuses_id[0]
+      @default_closed_status_id = @closed_statuses_id[0]
     end
   end
-  
-  
+
 end
