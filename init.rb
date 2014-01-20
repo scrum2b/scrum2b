514ebaf69106457023638b8691f29bb0b165a4d9
Tuanha <tuanha@ithanoi.com>
2014-01-20 17:06:24 +0700

    Update init.rb

--- init.rb
+++ init.rb
@@ -7,14 +7,14 @@ require 'redmine'
 
 Redmine::Plugin.register :scrum2b do
   name 'Scrum2B Plugin'
-  author 'Scrum2B'
+  author 'ScrumTobe Team'
   description %Q{A scrum tool for team to work:
                   - Scrum board
                   - Customize views
                 }
   version '2.0'
   url 'https://github.com/scrum2b/scrum2b'
-  author_url 'http://scrum2b.com'
+  author_url 'http://www.scrumtobe.com'
 
   settings :default => {'status_no_start'=> {}, 'status_inprogress' => {}, 'status_completed' => {}, 'status_closed' => {} }, :partial => 'settings/scrum2b'
   
